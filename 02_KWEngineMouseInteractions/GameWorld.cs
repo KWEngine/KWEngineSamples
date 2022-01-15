4c23b678fc1271db8f23285e0991db0c0bce41fc
Lutz Karau <karau_l@ers-hm.de>
2022-01-15 09:48:34 +0100

    added project #2 for mouse demonstrations

---
+++ 02_KWEngineMouseInteractions/GameWorld.cs
@@ -0,0 +1,25 @@
+﻿using KWEngine2;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_KWEngineMouseInteractions
+{
+    class GameWorld : World
+    {
+        public override void Act(KeyboardState ks, MouseState ms)
+        {
+
+        }
+
+        public override void Prepare()
+        {
+            KWEngine.LoadModelFromFile("ubot", @".\models\ubot.fbx");
+
+
+        }
+    }
+}
