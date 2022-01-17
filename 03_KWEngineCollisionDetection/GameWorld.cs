34bce216ddbfd06c7cc532390632317ec5f13d80
Lutz Karau <karau_l@ers-hm.de>
2022-01-17 06:48:41 +0100

    added 3rd world

---
+++ 03_KWEngineCollisionDetection/GameWorld.cs
@@ -0,0 +1,25 @@
+﻿using KWEngine2;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_KWEngineCollisionDetection
+{
+    class GameWorld : World
+    {
+        public override void Act(KeyboardState ks, MouseState ms)
+        {
+
+        }
+
+        public override void Prepare()
+        {
+            KWEngine.LoadModelFromFile("ubot", @".\models\ubot.fbx");
+
+
+        }
+    }
+}
---
+++ 03_KWEngineCollisionDetection/Player.cs
@@ -0,0 +1,22 @@
+﻿using KWEngine2;
+using KWEngine2.Collision;
+using KWEngine2.GameObjects;
+using KWEngine2.Helper;
+using OpenTK;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_KWEngineCollisionDetection
+{
+    class Player : GameObject
+    {
+        public override void Act(KeyboardState ks, MouseState ms)
+        {
+
+        }
+    }
+}
