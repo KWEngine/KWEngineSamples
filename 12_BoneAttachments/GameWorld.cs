11f061762b697318d210f3c455a4c51edfe033a8
Karau Lutz <lutz.karau@ers-hameln.de>
2022-04-25 11:09:08 +0200

    Finished world #12

--- 12_BoneAttachments/GameWorld.cs
+++ 12_BoneAttachments/GameWorld.cs
@@ -37,8 +37,11 @@ namespace _12_BoneAttachments
             sun.SetNearAndFarBounds(5, 100);
             AddLightObject(sun);
 
-            // Load 3D player model:
+            // Load 3D models:
             KWEngine.LoadModelFromFile("Ortiz", @".\models\ortiz.fbx");
+            KWEngine.LoadModelFromFile("Sword", @".\models\sword.glb");
+
+            //List<string> boneNames = KWEngine.GetModelBoneNames("Ortiz");
 
             // Initialize player object:
             Player p = new Player();
@@ -59,6 +62,34 @@ namespace _12_BoneAttachments
             f01.IsShadowCaster = true;
             f01.IsCollisionObject = true;
             AddGameObject(f01);
+
+            // Place sword:
+            Sword s01 = new Sword();
+            s01.SetModel("Sword");
+            s01.SetPosition(-2.5f, 0, 2.5f);
+            s01.SetRotation(90, 90, 0);
+            s01.IsCollisionObject = true;
+            s01.IsShadowCaster = true;
+            AddGameObject(s01);
+
+            // Create HUD objects:
+            HUDObject h1 = new HUDObject(HUDObjectType.Text, 32, 32);
+            h1.SetText("Use WASD to move.");
+            h1.SetFont(FontFace.XanhMono);
+            h1.CharacterSpreadFactor = 20;
+            AddHUDObject(h1);
+
+            HUDObject h2 = new HUDObject(HUDObjectType.Text, 32, 64);
+            h2.SetText("Use SPACE to pickup or drop weapon.");
+            h2.SetFont(FontFace.XanhMono);
+            h2.CharacterSpreadFactor = 20;
+            AddHUDObject(h2);
+
+            HUDObject h3 = new HUDObject(HUDObjectType.Text, 32, 96);
+            h3.SetText("Use MOUSELEFT to attack.");
+            h3.SetFont(FontFace.XanhMono);
+            h3.CharacterSpreadFactor = 20;
+            AddHUDObject(h3);
         }
     }
 }
--- 12_BoneAttachments/Player.cs
+++ 12_BoneAttachments/Player.cs
@@ -6,58 +6,166 @@ using OpenTK;
 using OpenTK.Input;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _12_BoneAttachments
 {
     internal class Player : GameObject
     {
-        private enum MovementType { Idle, Walk, Pickup, Attack };
+        private enum MovementType { Idle, Walk, Attack };
 
         private float _speed = 0.1f;
         private float _animPercent = 0.0f;
+        private MovementType _movementType = MovementType.Idle;
+        private bool _mouseLeftReleased = true;
+        private bool _spaceKeyReleased = true;
+
         public override void Act(KeyboardState ks, MouseState ms)
         {
-            MovementType movementType = MovementType.Idle;
+            bool animationJustSwitched = false;
 
-            if (ks.IsKeyDown(Key.W) && Position.Z > -5)
+            if(ms.IsButtonUp(MouseButton.Left))
             {
-                MoveOffset(0, 0, -_speed * KWEngine.DeltaTimeFactor);
-                movementType = MovementType.Walk;
+                _mouseLeftReleased = true;
             }
-            if (ks.IsKeyDown(Key.S) && Position.Z < 5)
+            if (ks.IsKeyUp(Key.Space))
             {
-                MoveOffset(0, 0, _speed * KWEngine.DeltaTimeFactor);
-                movementType = MovementType.Walk;
-            }
-            if (ks.IsKeyDown(Key.A) && Position.X > -5)
-            {
-                MoveOffset(-_speed * KWEngine.DeltaTimeFactor, 0, 0);
-                movementType = MovementType.Walk;
-            }
-            if (ks.IsKeyDown(Key.D) && Position.X < 5)
-            {
-                MoveOffset(_speed * KWEngine.DeltaTimeFactor, 0, 0);
-                movementType = MovementType.Walk;
+                _spaceKeyReleased = true;
             }
 
-            DoAnimation(movementType);
+            if (ms.IsButtonDown(MouseButton.Left))
+            {
+                _mouseLeftReleased = false;
+                if(_movementType != MovementType.Attack)
+                {
+                    _movementType = MovementType.Attack;
+                    animationJustSwitched = true;
+                }
+            }
+            else
+            {
+                TurnTowardsXZ(GetMouseIntersectionPoint(ms, Plane.Y, 0, 16));
+                _movementType = MovementType.Idle;
+
+                if (ks.IsKeyDown(Key.W) && Position.Z > -5)
+                {
+                    MoveOffset(0, 0, -_speed * KWEngine.DeltaTimeFactor);
+                    _movementType = MovementType.Walk;
+                }
+                if (ks.IsKeyDown(Key.S) && Position.Z < 5)
+                {
+                    MoveOffset(0, 0, _speed * KWEngine.DeltaTimeFactor);
+                    _movementType = MovementType.Walk;
+                }
+                if (ks.IsKeyDown(Key.A) && Position.X > -5)
+                {
+                    MoveOffset(-_speed * KWEngine.DeltaTimeFactor, 0, 0);
+                    _movementType = MovementType.Walk;
+                }
+                if (ks.IsKeyDown(Key.D) && Position.X < 5)
+                {
+                    MoveOffset(_speed * KWEngine.DeltaTimeFactor, 0, 0);
+                    _movementType = MovementType.Walk;
+                }
+            }
+
+            // Only pickup weapon, if there is no object attached to the player right now:
+            if(ks.IsKeyDown(Key.Space) && _spaceKeyReleased && HasAttachedGameObjects)
+            {
+                _spaceKeyReleased = false;
+                ThrowAwayObject();
+            }
+            
+
+            // If the player has an intersection with a sword and the sword is not held by the player already
+            // color the player object's outline red to show that the sword may be picked up now:
+            List<Intersection> intersections = GetIntersections();
+            foreach(Intersection i in intersections)
+            {
+                if(i.Object is Sword && HasAttachedGameObjects == false)
+                {
+                    SetColorOutline(1, 0, 0, 1);
+
+                    // If space key is pressed, the player picks it up:
+                    if(ks.IsKeyDown(Key.Space) && _spaceKeyReleased)
+                    {
+                        _spaceKeyReleased = false;
+                        PickupObject(i.Object);
+                        SetColorOutline(0, 0, 0, 0);
+                    }
+                }
+                else
+                {
+                    SetColorOutline(0, 0, 0, 0);
+                }
+            }
+            
+            // This controls the player animations:
+            DoAnimation(animationJustSwitched);
         }
 
-        private void DoAnimation(MovementType movementType, bool justSwitched = false)
+        private void PickupObject(GameObject o)
         {
-            if(movementType == MovementType.Idle)
+            AttachGameObjectToBone(o, "mixamorig:LeftHand");
+
+            // Note: If the model has no dedicated weapon bone, you may need to choose any bone
+            //       but this does not necessarily scale and rotate the attached object correctly.
+            //       In this case, you can use SetScale() etc. to finetune the attachment.
+            //       All parameters are not relative to the parent bone, not the actual world!
+            o.SetScale(1.5f);           // Scale the weapon to 1.5x its size
+            o.SetRotation(-45, 0, 90);  // Rotate the weapon in order for the Player object to hold it correctly
+            o.SetPosition(0, 0.05f, 0); // Move the weapon slightly along the bone's direction (not needed)
+        }
+
+        private void ThrowAwayObject()
+        {
+            // Get the object that the player is holding right now:
+            GameObject o = GetAttachedGameObjectForBone("mixamorig:LeftHand");
+
+            // Detach any object that is held in the left hand:
+            DetachGameObjectFromBone("mixamorig:LeftHand");
+
+            // If there really was an object in the player's left hand:
+            if(o != null)
+            {
+                // Place the sword object on the floor at the current player's position
+                // and reset its scale:
+                o.SetPosition(o.Position.X, 0, o.Position.Z);
+                o.SetRotation(90, 90, 0);
+                o.SetScale(1);
+            }
+        }
+
+        private void DoAnimation(bool justSwitched = false)
+        {
+            Console.WriteLine(_movementType);
+            if(_movementType == MovementType.Idle)
             {
                 AnimationID = 0;
                 _animPercent = (_animPercent + 0.005f * KWEngine.DeltaTimeFactor) % 1;
                 AnimationPercentage = _animPercent;
             }
-            else if(movementType == MovementType.Walk)
+            else if(_movementType == MovementType.Walk)
             {
                 AnimationID = 3;
                 _animPercent = (_animPercent + 0.025f * KWEngine.DeltaTimeFactor) % 1;
+                AnimationPercentage = _animPercent;
+            }
+            else if(_movementType == MovementType.Attack)
+            {
+                AnimationID = 7;
+                if (justSwitched)
+                {
+                    _animPercent = 0;
+                    Console.WriteLine("reset percent");
+                }
+                
+                _animPercent = _animPercent + 0.05f * KWEngine.DeltaTimeFactor;
+                
+                Console.WriteLine(_animPercent);
+                if (_animPercent >= 1)
+                {
+                    _movementType = MovementType.Idle;
+                }
                 AnimationPercentage = _animPercent;
             }
         }
